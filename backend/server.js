0d2073adb343962d93d14a06ed6eec67ae681989
TicTac14 <francisco.mendez114@gmail.com>
2023-11-01 13:41:04 -0400

    Added express backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("./public"));
+
+app.get('/', (req, res) => {
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+})
